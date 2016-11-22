8f362393e70184a7879024c6c3a85974f7d7f54f
Athandwaclear <siyasangalakhiwe6@gmail.com>
2016-11-22 12:51:49 +0200

    I have edited this file by adding values

--- firstPaarl.js
+++ firstPaarl.js
@@ -1,12 +1,12 @@
 function firstPaarl(str) {
-  var regList = str.split(',');
-  for(var i=0;i<regList.length;i++){
-if (regList[i].startsWith('CJ'));{
-      return regList[i];
+  var regList = str.split(', ');
+    for(var i=0; i<regList.length; i++){
+      if(regList[i].startsWith('CJ')){
+        return regList[i];
+      }
     }
-  }
 }
-console.log(firstPaarl('CY 10111, CJ 10299, CA 100010'));
+console.log(firstPaarl('DRT 4008 L, CJ 0438, CA 05408, CY 9808, ND 2347, CJ 8447'));
  // const assert = require('assert')
  //
  // assert.equal(firstPaarl('CY 10111, CA 10299, CJ 100010'), 'CA 10111');
