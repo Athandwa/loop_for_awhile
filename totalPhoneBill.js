44feb2b819519c13497028b167cb7e9aa3c0ec83
Athandwaclear <siyasangalakhiwe6@gmail.com>
2016-11-23 17:03:21 +0200

    initial commit on this file and tested with deepEqual

---
+++ totalPhoneBill.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+function totalPhoneBill(phoneBill) {
+    var billList = phoneBill.split(',');
+    var total = 0;
+
+  for(var i=0; i<billList.length; i++){
+
+    if(billList[i] === 'call'){
+        total = total + 2.75;
+      }
+    else if(billList[i] === 'sms'){
+        total = total + 0.65;
+    }
+  }
+        return total.toFixed(2);
+}
+//console.log(totalPhoneBill('call,sms,call,sms,sms'));
+var billList = totalPhoneBill('call,sms,call,sms,sms');
+console.log(billList);
+assert.deepEqual(billList, 7.45);
